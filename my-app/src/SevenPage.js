568efc3ce159344bc313e6bcea297fc98b3d57a4
Mystery-Oh <dghmltmd2760@gmail.com>
2025-05-23 03:26:46 +0900

    add_navbar to quiz
    퀴즈페이지에 네비게이션 바 추가, 한화 영상만 나오게 수정

--- my-app/src/SevenPage.js
+++ my-app/src/SevenPage.js
@@ -15,8 +15,37 @@ function SevenPage() {
     navigate('/eight', { state: { yesCount: prevCount } });
   };
 
+    // 홈으로 새로고침
+  const goHome = () => {
+    window.location.href = '/'; 
+  };
+
+    // 퀴즈 페이지로 이동
+  const goToQuiz = () => {
+    navigate('/one');
+  };
+
   //나는 혼자 돋보이는 것 보다 팀을위해 희생하는 것을 선호한다
   return (
+  <div>
+    <div id="header">
+        <div id="hd">
+          <img
+            src="bluecap.jpeg"
+            height="75px"
+            width="100"
+            alt="bluecap"
+            onClick={goHome}
+            style={{ cursor: 'pointer' }}
+          />
+          <ul id="navigation">
+            <li onClick={() => navigate('/board')}>Board</li>
+            <li onClick={goToQuiz}>Who is my favorite player?</li>
+            <li>game date</li>
+            <li onClick={() => navigate('/Login')}>Sign In</li>
+          </ul>
+        </div>
+      </div>
     <div className = "container">
     <div className = "page">
       <p style={{ textAlign: 'left' }}> Question.7</p>
@@ -24,6 +53,7 @@ function SevenPage() {
       <button className = "button" onClick={handleYes}>O</button>
       <button className = "button" onClick={handleNo}>X</button>
     </div>
+  </div>
   </div>
   );
 }
